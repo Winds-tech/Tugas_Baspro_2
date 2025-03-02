3c33c4f12a6d82c4a9cddbf127edda4466253f44
Ridho <65000261+Winds-tech@users.noreply.github.com>
2025-03-02 11:50:32 +0700

    VolumeTabung.py

--- VolumeTabung.py
+++ VolumeTabung.py
@@ -2,7 +2,8 @@ import math
 
 r = float(input(" Masukkan panjang jari-jari tabung: "))
 t = float(input(" Masukkan tinggi tabung: "))
+pi = 22/7
 
-v = math.pi * r * r * t 
+v = pi * r * r * t 
 
 print("Volume tabung dengan jari-jari", r, "dan tinggi", t, "adalah", v)
