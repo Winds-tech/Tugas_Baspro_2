4d9b7205cd53a39cb6af797d0552ea5d06d382ef
Ridho <65000261+Winds-tech@users.noreply.github.com>
2025-03-10 13:25:24 +0700

    Add files via upload

---
+++ Minggu 2/LuasLingkaranpi.py
@@ -0,0 +1,5 @@
+r = float(input(" Masukkan panjang jari-jari lingkaran: "))
+pi = 22/7
+
+luas = pi * r * r
+print("Luas lingkaran dengan jari-jari", r, "adalah", luas)
---
+++ Minggu 2/LuasPersegiPanjang.py
@@ -0,0 +1,6 @@
+p = (input("Masukkan panjang: "))
+l = (input("Masukkan lebar: "))
+
+luas = p * l
+
+print("Luas persegi panjang dengan panjang", p, "dan lebar", l, "adalah", luas)
